4df903d28f668a7429b11c7b13f4b661f5d3f513
Mario Córdoba <cord0388@gmail.com>
2024-01-03 13:37:07 -0300

    fixes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 function App() {
+  let main_text = "I'm heading back to Colorado tomorrow after being down in Santa Barbara over the weekend for the festival there. I will be making October plans once there and will try to arrange so I'm back here for the birthday if possible. I'll let you know as soon as I know the doctor's appointment schedule and my flight plans.";
+ 
   return (
     <div className="container">
       <div className="textBox">
         <p className="text">
-          I'm heading back to Colorado tomorrow after being down in Santa Barbara over the weekend for the festival there. I will be making October plans once there and will try to arrange so I'm back here for the birthday if possible. I'll let you know as soon as I know the doctor's appointment schedule and my flight plans.
+          {main_text}
         </p>
       </div>
     </div>
